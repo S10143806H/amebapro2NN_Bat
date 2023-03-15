a1393944a9d9b269820d9be9d69f0f49229acfca
Zhu Qi <zhuqi@realtek-sg.com>
2023-03-15 11:53:00 +0800

    * [Bug Fix] Unable to detect customized model folder: AmebaModel + Add compilation abortion feature

--- updatemodel_tool.py
+++ updatemodel_tool.py
@@ -11,8 +11,7 @@ import json
 from datetime import datetime, timezone
 from sys import platform
 
-DEBUG= False
-
+DEBUG = False
 def debug_print(message):
     if DEBUG:
         print(message)
@@ -21,38 +20,38 @@ def debug_print(message):
 usrmodel_path = os.path.abspath(os.path.dirname(sys.argv[0]))
 for file_user_model in os.listdir(usrmodel_path):
     if file_user_model.endswith(".nb"):
-        debug_print(os.path.join(usrmodel_path, file_user_model))
+        # debug_print(os.path.join(usrmodel_path, file_user_model))
         # Get the file name without the file type extension recursively
         file_user_model_no_ext = os.path.splitext(os.path.basename(file_user_model))[0]
-        debug_print(file_user_model_no_ext)
+        # debug_print(file_user_model_no_ext)
 
 if platform == "win32":
     # Windows...
     desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-    ameba_model_path = os.path.join(desktop, 'AmebaModel')
+    ameba_model_path = os.path.join(desktop, 'AmebaModel\\')
     debug_print(desktop)
     arduino15_path = os.path.expanduser("~\AppData\Local\Arduino15")
     ambpro2_path = arduino15_path + "\packages\\realtek\hardware\AmebaPro2"
     sdk_version = os.listdir(ambpro2_path)[0]
-    dest_path = ambpro2_path + "\\" + sdk_version + "\\variants\common_nn_models"
+    dest_path = ambpro2_path + "\\" + sdk_version + "\\variants\common_nn_models\\"
 elif platform == "linux" or platform == "linux2":
     # linux
     desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-    ameba_model_path = os.path.join(desktop, 'AmebaModel')
+    ameba_model_path = os.path.join(desktop, 'AmebaModel/')
     debug_print(desktop)
     arduino15_path = os.path.expanduser("/home/" + os.getlogin() + "/.arduino15")
     ambpro2_path = arduino15_path + "/packages/realtek/hardware/AmebaPro2/"
     sdk_version = os.listdir(ambpro2_path)[0]
-    dest_path = ambpro2_path + "/" + sdk_version + "/variants/common_nn_models"
+    dest_path = ambpro2_path + "/" + sdk_version + "/variants/common_nn_models/"
 elif platform == "darwin":
     # OS X
     desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-    ameba_model_path = os.path.join(desktop, 'AmebaModel')
+    ameba_model_path = os.path.join(desktop, 'AmebaModel/')
     debug_print(desktop)
     arduino15_path = os.path.expanduser("/Users/" + os.getlogin() + "/Library/Arduino15")
     ambpro2_path = arduino15_path + "/packages/realtek/hardware/AmebaPro2/"
     sdk_version = os.listdir(ambpro2_path)[1]
-    dest_path = ambpro2_path + "/" + sdk_version + "/variants/common_nn_models"
+    dest_path = ambpro2_path + "/" + sdk_version + "/variants/common_nn_models/"
 
 allowed_values = [
     "CYV3", "CYV4", "CYV7", "CM8", "CM16", "CS32", "CS64", 
@@ -61,7 +60,8 @@ allowed_values = [
 ]
 
 def dspFileProp(filename):
-    file_model_stats = os.stat(filename)
+    print(dest_path + filename)
+    file_model_stats = os.stat(dest_path + filename)
     file_model_datetime = datetime.fromtimestamp(file_model_stats.st_mtime, tz = timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
     file_model_date = datetime.fromtimestamp(file_model_stats.st_mtime, tz = timezone.utc).strftime('%Y-%m-%d')
     file_model_mode = oct(file_model_stats.st_mode)
@@ -74,10 +74,12 @@ def dspFileProp(filename):
     return file_model_date
 
 def renameFile(filename, type):
+    debug_print(dspFileProp(filename))
     if type == 1:
         # Backup Dmodel
         filename_modified = "Dbackup_" + dspFileProp(filename) + "_" + filename
         if os.path.isfile(os.path.join(dest_path, filename_modified)) == False:
+            debug_print(os.path.join(dest_path, filename_modified))
             os.rename(os.path.join(dest_path, filename), os.path.join(dest_path, filename_modified))
         debug_print("[INFO] Dmodel Backup done.")
     else:
@@ -88,35 +90,31 @@ def renameFile(filename, type):
         debug_print("[INFO] Cmodel Backup done.")
 
 def backupModel(user_model):
-    debug_print("[INFO] Backup Default Model: " + input2model(user_model))
+    debug_print("[INFO] Backup Default Model: " + input2model(user_model) + ".nb")
+
     for dest_file in os.listdir(dest_path):
         if "Dbackup" in dest_file:
             debug_print(f"[INFO] Backup Default Model {user_model} found !!!")
         elif input2model(user_model) in dest_file:
-            # backup default model
             renameFile(input2model(user_model) + ".nb", 1)
     
     # backup Cmodel
     if platform == "linux" or platform == "linux2" or platform == "darwin" : 
-            # linux & OS X
-        shutil.copy(usrmodel_path + "/" + input2model(user_model) + ".nb", dest_path)
-    #                     # elif platform == "darwin":
-    #                     #     # OS X
+        # linux & OS X
+        shutil.copy(ameba_model_path + "/" + input2model(user_model) + ".nb", dest_path)
     elif platform == "win32":
-    #                         # Windows...
-        shutil.copy(usrmodel_path + "\\" + input2model(user_model) + ".nb", dest_path)
+        # Windows...
+        shutil.copy(ameba_model_path + "\\" + input2model(user_model) + ".nb", dest_path)
     debug_print(input2model(user_model) + ".nb")
     renameFile(input2model(user_model) + ".nb", 0)
 
     # copy Cmodel
     if platform == "linux" or platform == "linux2" or platform == "darwin" : 
-            # linux & OS X
-        shutil.copy(usrmodel_path + "/" + input2model(user_model) + ".nb", dest_path)
-    #                     # elif platform == "darwin":
-    #                     #     # OS X
+        # linux & OS X
+        shutil.copy(ameba_model_path + "/" + input2model(user_model) + ".nb", dest_path)
     elif platform == "win32":
-    #                         # Windows...
-        shutil.copy(usrmodel_path + "\\" + input2model(user_model) + ".nb", dest_path)
+        # Windows...
+        shutil.copy(ameba_model_path + "\\" + input2model(user_model) + ".nb", dest_path)
     debug_print("[INFO] User model copied.")
 
 def revertModel(user_model):
@@ -195,36 +193,36 @@ def input2model(input):
     model = model_mapping.get(input)
     return model
 
+def folderValidationCheck():
+    # TODO: customized folder validation
+    if not os.path.exists(ameba_model_path):
+        sys.stderr.write("[Error] AmebaModel folder does not exist on the Desktop.\n")
+        sys.exit(1)
+    else:
+        if not os.listdir(ameba_model_path):
+            sys.stderr.write("[Error] AmebaModel folder empty. Please put your customized model inside.\n")
+            sys.exit(1)
+
 def validationCheck(input):
     if input in allowed_values:
         if input[0] == 'C':
             # convert input to model
             folderValidationCheck()
-            if os.path.isfile(input2model(input) + ".nb"):
-                debug_print(f"[INFO] Customized Model {input} Found!")
+            if os.path.isfile(ameba_model_path + input2model(input) + ".nb"):
+                debug_print(f"[INFO] Customized Model {input2model(input)} Found!")
                 return 1
             else:
-                debug_print(f"[Error] Model {input} NOT Found! Please check your input again.")
-                raise SystemExit
+                sys.stderr.write(f"[Error] Model {input2model(input)} NOT Found! Please check your input again.\n")
+                sys.exit(1)
         if input[0] == 'D':
             debug_print(f"[INFO] Default Model is being selected!")
             return 0
         else:
             raise SystemExit
     else:
-        debug_print("[Error] Please enter a valid input.")
+        # debug_print("[Error] Please enter a valid input.\n")
         raise SystemExit
 
-def folderValidationCheck():
-    # TODO: customized folder validation
-    if not os.path.exists(ameba_model_path):
-        print("The AmebaModel folder does not exist on the desktop.")
-        raise SystemExit
-    else:
-        if not os.listdir(ameba_model_path):
-            print("The AmebaModel folder is empty.")
-            raise SystemExit
-    
 if __name__ == '__main__':
     user_input_flag = False
     while user_input_flag == False:
